8d0a006ac7bc53c00db8761d65c7eaa9832f0636
Andrea Burattin <delas@users.noreply.github.com>
2016-11-18 14:36:15 +0000

    * Added selection of the CSV file, for #622 * Factorized loader (to implement additional ones)

--- src/moderare/correlations2/test/Tester.java
+++ src/moderare/correlations2/test/Tester.java
@@ -8,26 +8,21 @@ import moderare.correlations2.model.Entry;
 public class Tester {
 
 	public static void main(String[] args) throws Exception {
-		CSVLoader l = new CSVLoader("C:\\Users\\Andrea\\Desktop\\behavior-pragmatic.csv");
+		CSVLoader l = new CSVLoader();
+		l.loadFile("C:\\Users\\Andrea\\Desktop\\behavior-pragmatic.csv");
 		Dataset d = l.exportDataset();
 
-//		System.out.println(d.size());
+		System.out.println(d.size());
 		System.out.println(d.filter("expertise = \"experts\" and No_homogeneous_fragments != 1").size());
-//		System.out.println(d.keepDistinct(new Entry("expertise", "experts")).size());
-//		System.out.println(d.filterAny(new Entry("expertise", "familiar")).size());
-//		System.out.println(d.filterAny(new Entry("expertise", "experts"), new Entry("expertise", "familiar")).size());
-//		System.out.println(d.filterAll(new Entry("expertise", "a")).size());
-//		
-//		System.out.println("---");
-//		System.out.println(d.getDistinctValues("expertise"));
-//		System.out.println(d.getAllValues("expertise"));
-//		
-//		System.out.println("---");
-//		System.out.println(d.slice("expertise", "percent_orthogonal_segments").size());
+		System.out.println(d.keepEqual(new Entry("expertise", "experts")).size());
+		System.out.println(d.keepEqual(new Entry("expertise", "familiar")).size());
+		System.out.println(d.keepEqual(new Entry("expertise", "a")).size());
 		
-//		CorrelationsComputer cc = new CorrelationsComputer(d.filterAll(new Entry("expertise", "unfamiliar")));
-//		CorrelationsComputer cc = new CorrelationsComputer(d.keepDistinct(new Entry("expertise", "unfamiliar")));
-		CorrelationsComputer cc = new CorrelationsComputer(d);
+		System.out.println("---");
+		System.out.println(d.getDistinctValues("expertise"));
+		System.out.println(d.getAllValues("expertise"));
+		
+		CorrelationsComputer cc = new CorrelationsComputer(d.keepEqual(new Entry("expertise", "unfamiliar")));
 		System.out.println(cc.getCorrelation("percent_acts_in_mixed_fragments_aligned", "sum_handles_free_choice"));
 	}
 
