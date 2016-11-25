b6aebf027dc4ed1578c57a439b0fc6aa6011f299
Andrea Burattin <delas@users.noreply.github.com>
2016-11-25 08:51:31 +0000

    Added possibility to select rows/columns attributes

--- src/moderare/correlations/CorrelationsInspector.java
+++ src/moderare/correlations/CorrelationsInspector.java
@@ -1,6 +1,7 @@
 package moderare.correlations;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.swing.JFileChooser;
@@ -9,6 +10,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 import moderare.correlations.loader.CSVLoader;
 import moderare.correlations.loader.Loader;
+import moderare.correlations.model.Dataset;
+import moderare.correlations.ui.AttributesDialog;
 import moderare.correlations.ui.CorrelationsInspectorFrame;
 
 public class CorrelationsInspector {
@@ -27,41 +30,24 @@ public class CorrelationsInspector {
 			}
 			datasetFile = fc.getSelectedFile().getAbsolutePath();
 		}
-		
-		List<String> rows = new LinkedList<String>();
-		rows.add("no_Dead_transitions");
-		rows.add("no_Non_live_transitions");
-		rows.add("no_Unbounded_places");
-		rows.add("no_PThandles");
-		rows.add("no_TPhandles");
-		rows.add("sum_handles");
-		rows.add("no_Freechoice_violations");
-		rows.add("sum_handles_free_choice");
-		
-		List<String> columns = new LinkedList<String>();
-		columns.add("Alignment_of_elements");
-		columns.add("Alignment_of_gateways");
-		columns.add("No_homogeneous_fragments");
-		columns.add("No_mixed_fragments");
-		columns.add("Alignment_of_fragments_with_explicit_gateways");
-		columns.add("Alignment_of_fragments_with_implicit_gateways");
-		columns.add("percent_acts_in_mixed_fragments");
-		columns.add("percent_acts_in_mixed_fragments_aligned");
-		columns.add("percent_acts_in_mixed_fragments_not_aligned");
-		columns.add("percent_acts_in_homogeneous_fragments");
-		columns.add("percent_acts_in_homogeneous_fragments_aligned");
-		columns.add("percent_acts_in_homogeneous_fragments_not_aligned");
-		columns.add("percent_crossing_edges");
-		columns.add("percent_orthogonal_segments");
-		columns.add("M_BP");
-		
+
 		Loader l = new CSVLoader();
 		l.loadFile(datasetFile);
-		CorrelationsInspectorFrame frame = new CorrelationsInspectorFrame(l.exportDataset(), rows, columns);
+		Dataset d = l.exportDataset();
+		
+		List<String> attributes = new ArrayList<String>(d.getAttributes());
+		Collections.sort(attributes);
+		
+		AttributesDialog rowsDialog = new AttributesDialog("Select rows attributes", attributes);
+		rowsDialog.setVisible(true);
+		AttributesDialog columnsDialog = new AttributesDialog("Select column attributes", attributes);
+		columnsDialog.setVisible(true);
+		
+		CorrelationsInspectorFrame frame = new CorrelationsInspectorFrame(d, rowsDialog.getSelectedAttributes(), columnsDialog.getSelectedAttributes());
 		frame.setDefaultCloseOperation(CorrelationsInspectorFrame.EXIT_ON_CLOSE);
 		frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 		frame.setSize(800, 800);
-		frame.setTitle("MODERARE - " + datasetFile);
+		frame.setTitle("CorrelationsInspector - " + datasetFile);
 		frame.setVisible(true);
 	}
 }
