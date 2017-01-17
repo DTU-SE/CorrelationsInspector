7a14fa28ee93d02e6cebb07f9666d9d6e052a9f9
andrea.burattin <andrea.burattin@7ae91e77-2e1c-4327-9467-692330935c57>
2017-01-17 15:07:32 +0000

    Added possibility to choose the correlation type, for #654

--- trunk/code/moderare/CorrelationsInspector/src/moderare/correlations/test/Tester.java
+++ trunk/code/moderare/CorrelationsInspector/src/moderare/correlations/test/Tester.java
@@ -1,6 +1,7 @@
 package moderare.correlations.test;
 
 import moderare.correlations.calculators.CorrelationsComputer;
+import moderare.correlations.calculators.CorrelationsComputer.CORRELATION_TYPE;
 import moderare.correlations.expressions.ExpressionFilter.OPERATOR;
 import moderare.correlations.loader.CSVLoader;
 import moderare.correlations.model.Dataset;
@@ -10,12 +11,12 @@ public class Tester {
 
 	public static void main(String[] args) throws Exception {
 		CSVLoader l = new CSVLoader();
-		l.loadFile("C:\\Users\\Andrea\\Desktop\\behavior-pragmatic.csv");
+		l.loadFile("C:\\Users\\Andrea\\Desktop\\behavioral-pragmatic.csv");
 		Dataset d = l.exportDataset();
 
 		System.out.println("---");
 		System.out.println(d.size());
-		System.out.println(d.filter("expertise = \"experts\" and No_homogeneous_fragments != 1").size());
+		System.out.println(d.filter("expertise = \"experts\" & No_homogeneous_fragments != 1").size());
 		System.out.println(d.filter(OPERATOR.EQUAL, new Entry("expertise", "experts")).size());
 		System.out.println(d.filter(OPERATOR.EQUAL, new Entry("expertise", "familiar")).size());
 		System.out.println(d.filter(OPERATOR.EQUAL, new Entry("expertise", "a")).size());
@@ -26,7 +27,7 @@ public class Tester {
 		
 		System.out.println("---");
 		CorrelationsComputer cc = new CorrelationsComputer(d.filter(OPERATOR.EQUAL, new Entry("expertise", "unfamiliar")));
-		System.out.println(cc.getCorrelation("percent_acts_in_mixed_fragments_aligned", "sum_handles_free_choice"));
+		System.out.println(cc.getCorrelation("percent_acts_in_mixed_fragments_aligned", "sum_handles_free_choice", CORRELATION_TYPE.PEARSONS));
 		
 		System.out.println("---");
 		System.out.println(d.filter(OPERATOR.LESS_EQUAL, new Entry("no_Dead_transitions", 10d)).size());
--- trunk/code/moderare/CorrelationsInspector/src/moderare/correlations/ui/CorrelationsInspectorFrame.java
+++ trunk/code/moderare/CorrelationsInspector/src/moderare/correlations/ui/CorrelationsInspectorFrame.java
@@ -2,11 +2,14 @@ package moderare.correlations.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -15,6 +18,7 @@ import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import moderare.correlations.calculators.CorrelationsComputer.CORRELATION_TYPE;
 import moderare.correlations.model.Dataset;
 import moderare.correlations.ui.widgets.tabs.ClosableJTabbedPane;
 import moderare.correlations.ui.widgets.tabs.CorrelationTab;
@@ -27,6 +31,9 @@ public class CorrelationsInspectorFrame extends JFrame {
 	// gui objects
 	private JTextField filter = new JTextField();
 	private ClosableJTabbedPane tabs = new ClosableJTabbedPane();
+	private JComboBox<String> correlations = new JComboBox<>(new String[] {
+			CORRELATION_TYPE.PEARSONS.name(),
+			CORRELATION_TYPE.SPERMANS.name()});
 	
 	// logic objects
 	private Dataset original;
@@ -59,14 +66,22 @@ public class CorrelationsInspectorFrame extends JFrame {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-					newFormula(filter.getText().trim());
+					newFormula(getFormula(), getCorrelationType());
 				}
+			}
+		});
+		
+		correlations.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				newFormula(getFormula(), getCorrelationType());
 			}
 		});
 		
 		JPanel filterPanel = new JPanel(new BorderLayout(5, 5));
 		filterPanel.add(new JLabel("Filter: "), BorderLayout.WEST);
 		filterPanel.add(filter, BorderLayout.CENTER);
+		filterPanel.add(correlations, BorderLayout.EAST);
 		add(filterPanel, BorderLayout.NORTH);
 		
 		tabs.addChangeListener(new ChangeListener() {
@@ -87,14 +102,23 @@ public class CorrelationsInspectorFrame extends JFrame {
 		
 		// show inital tabs
 		tabs.add("Dataset summary", new SummaryTab(original));
-		newFormula("");
+		correlations.setSelectedItem(CORRELATION_TYPE.SPERMANS.name());
+	}
+	
+	private String getFormula() {
+		return filter.getText().trim();
+	}
+	
+	private CORRELATION_TYPE getCorrelationType() {
+		return CORRELATION_TYPE.valueOf((String) correlations.getSelectedItem());
 	}
 	
 	/**
 	 * 
 	 * @param formula
+	 * @param type 
 	 */
-	private void newFormula(String formula) {
+	private void newFormula(String formula, CORRELATION_TYPE type) {
 		try {
 			String title = "Original datset";
 			Dataset newDataset = original;
@@ -102,8 +126,8 @@ public class CorrelationsInspectorFrame extends JFrame {
 				newDataset = original.filter(formula);
 				title = formula;
 			}
-			title += " (" + newDataset.size() + " entries)";
-			tabs.add(title, new CorrelationTab(formula, newDataset, rows, columns));
+			title += " (" + newDataset.size() + " entries, " + type.name().toLowerCase() + " correlation)";
+			tabs.add(title, new CorrelationTab(formula, newDataset, rows, columns, type));
 			tabs.setSelectedIndex(tabs.getTabCount() - 1);
 		} catch (Exception e) {
 			filter.selectAll();
